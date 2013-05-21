ab9ebf30c2a8e4555022a0171ebb5d2a8a79dcaa
Matt Bierner <mattbierner@gmail.com>
2013-05-20 19:50:01 -0700

    V1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
 # ChangeLog #
 
-## 0.1.0 - March 2, 2013 ##
-* Initial release.
+## 1.0.0 - May 20, 2013 ##
+* Removed seal and freeze which don't make much sense for nonmutable objects.
 
 ## 0.2.0 - April 1, 2013 ##
 * Added delete property.
+
+## 0.1.0 - March 2, 2013 ##
+* Initial release.
--- test/testObject.js
+++ test/testObject.js
@@ -22,7 +22,7 @@ function(object){
                     }
                 }));
                 
-                assert.throws(object.defineProperty.bind(undefined, a, 'x', {'value': 7, 'enumerable': true}));
+                assert.deepEqual(object.defineProperty(a, 'x', {'value': 7, 'enumerable': true}), {'x': 7});
                 assert.deepEqual(a, {x: 3});
             }],
             
@@ -39,39 +39,6 @@ function(object){
                 
                 assert.deepEqual(object.deleteProperty(a, 'x'), {y: 4});
                 assert.deepEqual(a, {x: 3, y: 4});
-            }],
-            
-            ["Freeze",
-            function(){
-                var a = {x: 3, y: {z: 4}};
-                
-                var r = object.freeze(a);
-                assert.deepEqual(r, {x: 3, y: {z: 4}});
-                assert.deepEqual(a, {x: 3, y: {z: 4}});
-                assert.equal(Object.isFrozen(a), false);
-                assert.equal(Object.isFrozen(r), true);
-            }],
-            
-            ["Seal",
-            function(){
-                var a = {x: 3, y: {z: 4}};
-                
-                var r = object.seal(a);
-                assert.deepEqual(r, {x: 3, y: {z: 4}});
-                assert.deepEqual(a, {x: 3, y: {z: 4}});
-                assert.equal(Object.isSealed(a), false);
-                assert.equal(Object.isSealed(r), true);
-            }],
-            
-            ["Prevent Extensions",
-            function(){
-                var a = {x: 3, y: {z: 4}};
-                
-                var r = object.preventExtensions(a);
-                assert.deepEqual(r, {x: 3, y: {z: 4}});
-                assert.deepEqual(a, {x: 3, y: {z: 4}});
-                assert.equal(Object.isExtensible(a), true);
-                assert.equal(Object.isExtensible(r), false);
             }],
         ],
     };
